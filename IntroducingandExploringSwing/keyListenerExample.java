4b299495cb8e3ada408216777eea33adf5952f2e
Gaurav Shrestha <87366731+shrestha-gaurav@users.noreply.github.com>
2023-06-26 08:36:32 +0545

    Update keyListenerExample.java

--- IntroducingandExploringSwing/keyListenerExample.java
+++ IntroducingandExploringSwing/keyListenerExample.java
@@ -33,8 +33,8 @@ public class keyListenerExample extends JFrame implements KeyListener {
        if(e.getKeyCode()==KeyEvent.VK_ENTER)
        {
            txtSecond.setText(txtFirst.getText());
-           txtFirst.requestFocus();
-           txtFirst.selectAll();
+           txtSecond.requestFocus();
+           txtSecond.selectAll();
        }
        
     }
